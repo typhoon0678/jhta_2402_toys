79d860367ca28a1daaf51ff2250535037f9cbb71
typhoon0678 <typhoon0678@gmail.com>
2024-08-02 14:25:48 +0900

    level2 작업 중
    mariaDB 연결 작업 중

---
+++ toy_quest_02/src/MybatisConnectionFactory.java
@@ -0,0 +1,27 @@
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MybatisConnectionFactory {
+
+    private static final SqlSessionFactory sqlSessionFactory;
+
+    static {
+        String resource = "config.xml";
+
+        try {
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static SqlSession getSqlSession() {
+        return sqlSessionFactory.openSession(true);
+    }
+}
